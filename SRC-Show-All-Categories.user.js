c3407e496ff14bd5ff873f31fcca0cd4bc011e30
OceanBagel <50093445+OceanBagel@users.noreply.github.com>
2021-11-22 10:44:28 -0600

    Fixed the match for GreaseMonkey

--- SRC-Show-All-Categories.user.js
+++ SRC-Show-All-Categories.user.js
@@ -4,7 +4,7 @@
 // @version      1.0.0
 // @description  Userscript for showing all categories on speedrun.com
 // @author       OceanBagel
-// @match        *.speedrun.com/*
+// @match        https://www.speedrun.com/*
 // @icon         https://drive.google.com/uc?id=1evLQo0tDq48SnI_tLlXEeofU_kQ5Mcbd&export=download
 // @homepageURL  https://github.com/OceanBagel/SRC-Show-All-Categories
 // @downloadURL  https://github.com/OceanBagel/SRC-Show-All-Categories/raw/main/SRC-Show-All-Categories.user.js
